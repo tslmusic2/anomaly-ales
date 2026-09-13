b4747364547103619cbb06c76527be5d52a1066b
tslmusic2 <tslmusic2@gmail.com>
2026-09-13 10:24:01 -0400

    worked on adding query params to GET request on server.js.  cant tell if its working or not.  the code looks good but the git curl request doesnt finish executing

---
+++ public/frontend.js
@@ -0,0 +1,10 @@
+
+
+
+/*   render html section
+<article class="task" data-id="3">
+    <p>Backup computer</p>
+    <button class="complete-btn">Complete</button>
+    <button class="delete-btn">Delete</button>
+</article>
+*/
--- server.js
+++ server.js
@@ -27,8 +27,33 @@ console.log(req.method, req.url)
       if (req.url === '/api/tasks' && req.method === 'GET') {
 
             try {
+                  
+                  
+                  //const url = new URL(req.url, `http://${req.headers.host}`)
+
+                  //const priority = url.searchParams.get('priority')
+                  //const completed = url.searchParams.get('completed')
+
+
                   const tasksFile = await fs.readFile(tasksFilePath, 'utf8')
                   const parsedTasksFile = JSON.parse(tasksFile)
+
+            /*
+                  let tasksToSend = parsedTasksFile
+                  if (priority) {
+                        tasksToSend = tasksToSend.filter(task => {
+                              return task.priority.toLowerCase() === priority.toLowerCase()
+                        })
+                  }
+
+
+                  if (completed !== null) {
+                        const completedBoolean = completed === 'true'
+                        tasksToSend = tasksToSend.filter(task => {
+                              return task.completed === completedBoolean
+                        })
+                  }
+            */
 
                   sendResponse(
                         res,
@@ -99,13 +124,89 @@ console.log(req.method, req.url)
       }
 
 
+      if(req.url.startsWith('/api/tasks/') && req.method === 'PATCH') {
+
+            try {
+                  const parsedReqBody = await readRequestBody(req)
+
+
+                  const id = Number(req.url.split('/').pop())
+
+
+
+                  const tasksDataFile = await fs.readFile(tasksFilePath, 'utf8')
+                  const parsedtasksDataFile = JSON.parse(tasksDataFile)
+
+
+                  const patchTask = parsedtasksDataFile.find(task => task.id === id)         
+
+
+                  Object.assign(patchTask, parsedReqBody)
+
+
+                  await fs.writeFile(tasksFilePath, JSON.stringify(parsedtasksDataFile, null, 2), 'utf8')
+
+                  return sendResponse(
+                        res,
+                        200,
+                        'application/json',
+                        JSON.stringify({message: 'Your data update was successful'})
+                  )
+
+            } catch(err) {
+                  console.log(err) 
+                  return sendResponse(
+                        res,
+                        500,
+                        'application/json',
+                        JSON.stringify({error: 'There was an issue with the server'})
+                  )
+            }
+
+      }
+      
+
+
+      if (req.url.startsWith('/api/tasks/') && req.method === 'DELETE') {
+
+
+            try {
+
+                  const id = Number(req.url.split('/').pop())
+
+
+                  const tasksFile = await fs.readFile(tasksFilePath, 'utf8')
+                  const parsedTasksFile = JSON.parse(tasksFile)
+
+                  const updatedparsedTaskFile = parsedTasksFile.filter(task => task.id !== id)
+
+                  await fs.writeFile(tasksFilePath, JSON.stringify(updatedparsedTaskFile, null, 2), 'utf8')
+
+                  return sendResponse(
+                        res,
+                        200,
+                        'application/json',
+                        JSON.stringify({message:'The task was deleted successfully'})
+                  )
+            
+            } catch(err) {
+                  console.log(err) 
+                  sendResponse(
+                        res,
+                        500,
+                        'application/json',
+                        JSON.stringify({error: 'Unable to delete item'})
+                  )
+            }
+
+      }
 
 
 
       //      --APIs to build--
-      //GET --DONE    /api/tasks  add query param capabilities?
+      //GET   /api/tasks  add query param capabilities?
       //POST   /api/tasks  ----------DONE
-      //PATCH  /api/tasks/:id
+      //PATCH  /api/tasks/:id     ----------DONE
       //DELETE /api/tasks/:id
 
 
