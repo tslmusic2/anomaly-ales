5736de22108098daf9276e2a31003c442db9840c
tslmusic2 <tslmusic2@gmail.com>
2026-09-15 20:48:56 -0400

    initial commit, deleted some files used from repo i cloned

--- utilities/sendResponse.js
+++
@@ -1,5 +0,0 @@
-export function sendResponse(res, statusCode, contentType, data) {
-        res.statusCode = statusCode,
-        res.setHeader('Content-Type', contentType),
-        res.end(data)
-}
