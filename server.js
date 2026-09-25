4e7b261ddcd1455b084faf49c565a548812c7d85
tslmusic2 <tslmusic2@gmail.com>
2026-09-24 22:02:52 -0400

    finished PATCH api with tons of validation in it.  will dry ot the code later with utility functions.  all tests passed

--- server.js
+++ server.js
@@ -193,14 +193,37 @@ const server = http.createServer(async (req, res) => {
 
 			const parsedReqBody = await getRequestBody(req)
 
-
+			//includes ALL fields that can be accepted and altered in the table
 			const allowedFields = ['product_type', 'img_url', 'name', 'beer_style', 'abv', 'packaging_type', 'size', 'quantity', 'unit_price', 'is_active']
 			
+			//Reject null, non-object values, and arrays.
+			if ( parsedReqBody === null || typeof parsedReqBody !== 'object' || Array.isArray(parsedReqBody)) {
+				return sendResponse(
+					res,
+					400,
+					'application/json',
+					JSON.stringify({ message: 'Request body must be a JSON object' })
+				)
+			}
 
+			//Gets the keys out of the parsedReq array into an array of strings
+			//Then makes sure theres something in the array to be updated
 			const reqBodyArr = Object.keys(parsedReqBody)
 
-			//!!!!!!!!!!!!!NEED TO FIX THIS VALIDATION SECTION!!!!!!!!!!!!!!
-			if (!allowedFields.includes(reqBodyArr.key) || reqBodyArr.some(key => key != allowedFields.value)) {
+			if (reqBodyArr.length === 0) {
+				return sendResponse(
+					res,
+					400,
+					'application/json',
+					JSON.stringify({ message: 'Provide at least one field to update' })
+				)
+			}
+
+
+			//Looks through the req body for a field that isnt accepted
+			const hasInvalidField = reqBodyArr.some(field => !allowedFields.includes(field))
+
+			if (hasInvalidField) {
 				return sendResponse(
 					res,
 					400,
@@ -209,28 +232,161 @@ const server = http.createServer(async (req, res) => {
 				)
 			}
 
-			
 
-			
-			try {
+			//PRODUCT_TYPE  validates the product is beer or merchandise
+			const hasProductType = Object.hasOwn(parsedReqBody, 'product_type')
+			const productTypeValue = parsedReqBody.product_type
+			if(hasProductType) {
+				if (productTypeValue !== 'beer' && productTypeValue !== 'merchandise') {
+					return sendResponse(
+							res,
+							400,
+							'application/json',
+							JSON.stringify({ message: 'Invalid product type' })
+						)
+				}
+			}
 
-				const result = await pool.query(`
-					UPDATE quantity FROM inventory
-						WHERE quantity = $1
-					`)
+			//IMG_URL  validates the img_url
+			const hasImgUrl = Object.hasOwn(parsedReqBody, 'img_url')
+			const imgUrlValue = parsedReqBody.img_url
+			if(hasImgUrl) {
+				if (imgUrlValue !== null && (typeof imgUrlValue !== 'string' || imgUrlValue.trim().length === 0)) {
+					return sendResponse(
+							res,
+							400,
+							'application/json',
+							JSON.stringify({ message: 'Image URL must be a nonempty string or null' })
+						)
+				}
+			}
 
-			} catch (err) {
-				if (err.code === '23503') {
+			//NAME  validates the name
+			const hasName = Object.hasOwn(parsedReqBody, 'name')
+			const nameValue = parsedReqBody.name
+			if (hasName) {
+				if (typeof nameValue !== 'string' || nameValue.trim().length === 0) {
+					return sendResponse(
+							res,
+							400,
+							'application/json',
+							JSON.stringify({ message: 'Name must be a non empty string' })
+						)
+				}
+			}
+
+			//ABV  validates abv
+			const hasAbv = Object.hasOwn(parsedReqBody, 'abv')
+			const abvValue = parsedReqBody.abv
+
+			if(hasAbv) {
+				if(abvValue !== null && (!Number.isFinite(abvValue) || abvValue < 0 || abvValue > 99)) {
+					return sendResponse(
+							res,
+							400,
+							'application/json',
+							JSON.stringify({ message: 'ABV must be a number between 0 and 99, or null' })
+						)
+				}
+			}
+
+
+			//validates beer_style, packaging_type, size   
+			const stylePackSizeArr = ['beer_style', 'packaging_type', 'size']
+
+			for (const field of stylePackSizeArr) {
+				const fieldExists = Object.hasOwn(parsedReqBody, field)
+				const value = parsedReqBody[field]
+				
+				if(fieldExists) {
+					if(value !== null && (typeof value !== 'string' || value.trim().length === 0)) {
+						return sendResponse(
+						res,
+						400,
+						'application/json',
+						JSON.stringify({ message: `${field} must be a non empty string or null` })
+					)
+					}
+				}
+			}
+
+			//QUANTITY   validates a quantity if there is one.  That its an integer between 0 and 2147483647
+			const hasQuantity = Object.hasOwn(parsedReqBody, 'quantity')
+
+			if (hasQuantity) {
+				const quantityValue = parsedReqBody.quantity
+				if (!Number.isInteger(quantityValue) || quantityValue < 0 || quantityValue > 2147483647) {
 					return sendResponse(
 						res,
-						409,
+						400,
 						'application/json',
-						JSON.stringify({message: 'This product is referenced by an order and cannot be deleted.'})
+						JSON.stringify({ message: 'Quantity must be an integer between 0 and 2147483647' })
 					)
 				}
-
-				throw err
 			}
+
+			//UNIT_PRICE  validates unit_price
+			const hasUnitPrice = Object.hasOwn(parsedReqBody, 'unit_price')
+			const unitPriceValue = parsedReqBody.unit_price
+
+			if(hasUnitPrice) {
+				if(!Number.isFinite(unitPriceValue) || unitPriceValue < 0 || unitPriceValue > 99999999.99) {
+					return sendResponse(
+							res,
+							400,
+							'application/json',
+							JSON.stringify({ message: 'Unit price must be a valid number' })
+						)
+				}
+			}
+
+
+			const hasIsActive = Object.hasOwn(parsedReqBody, 'is_active')
+			const isActiveValue = parsedReqBody.is_active
+			if (hasIsActive) {
+				if (typeof isActiveValue !== 'boolean') {
+					return sendResponse(
+						res,
+						400,
+						'application/json',
+						JSON.stringify({ message: 'Value must be true or false' })
+					)
+				}
+			}
+
+//--------------------SQL query code HERE----------------------//
+
+
+			const assignments = reqBodyArr.map((field,index) => `${field} = $${index + 1}`)
+
+			const values = reqBodyArr.map(field => parsedReqBody[field])
+			values.push(id)
+			
+
+			const query = `
+				UPDATE inventory
+				SET ${assignments.join(', ')}
+				WHERE id = $${values.length}
+				RETURNING *;
+			`
+
+			const result = await pool.query(query, values)
+
+			if (result.rowCount === 0) {
+				return sendResponse(
+					res,
+					404,
+					'application/json',
+					JSON.stringify({ message: 'ID not found' })
+				)
+			}
+
+			return sendResponse(
+				res,
+				200,
+				'application/json',
+				JSON.stringify({message: 'Product updated successfully',product: result.rows[0]})
+			)
 
 
 		}
